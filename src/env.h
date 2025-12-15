3f4b7233bb6495da8d171ae4acab7241ceb0ef1d
choidh88 <dev.cdh88@gmail.com>
2025-12-15 18:50:34 +0900

    separated from one env.h to env.h and env.c

--- src/env.h
+++ src/env.h
@@ -1,18 +1,8 @@
 #ifndef __ENV_H__
 #define __ENV_H__
 
-#include <stdlib.h>
+char *getMessageEnv();
 
-char *getMessageEnv() {
-    char *result = getenv("MESSAGE");
-    if (result == NULL) return "(THE MESSAGE IS EMTPY)";
-    return result;
-}
-
-char *getUserEnv() {
-    char *result = getenv("USER");
-    if (result == NULL) return "(USER IS UNKNOWN)";
-    return result;
-}
+char *getUserEnv();
 
 #endif
